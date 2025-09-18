47a644775850c3f650beae7ed72a4ac5787810dc
matthew-hajec <matthajec@pm.me>
2025-09-18 09:40:00 -0400

    adjust GameView scroller height

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,3 +1,3 @@
 export default function Game() {
-    return <div style={{ width: "150vw", height: "100%" }}>Game Component</div>;
+    return <div style={{ width: "150vw", height: "100%", background: "linear-gradient(to right, #ff7e5f, #feb47b)" }}>Game Component</div>;
 }
